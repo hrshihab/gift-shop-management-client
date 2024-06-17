d8f1dc294c527d7aede7885c41ab99bf8aba1972
hrshihab <hrshihab10@gmail.com>
2024-06-17 22:47:05 +0600

    cart updated

--- src/utils/sideBarItemsGenerator.tsx
+++ src/utils/sideBarItemsGenerator.tsx
@@ -1,42 +1,4 @@
 import { TMenuItem, TUserPath } from "../types";
-// import { TMenuItem, TSidebarItem, TUserPath } from "../types";
-// import { NavLink } from "react-router-dom";
-
-// export const sideBarItemsGenerator = (items: TUserPath[], role: string) => {
-//     const sidebarItems = items.reduce((acc: TSidebarItem[], item) => {
-//         if (item.path && item.name) {
-//             acc.push({
-//                 key: item.name || (item.path as string),
-//                 label: (
-//                     <NavLink to={`/${role}/${item.path}`}>{item.name}</NavLink>
-//                 ),
-//             });
-//         }
-
-//         if (item.children) {
-//             acc.push({
-//                 key: item.name || (item.path as string),
-//                 label: item.name,
-//                 children: item.children.map((child) => {
-//                     if (child.name) {
-//                         return {
-//                             key: child.name,
-//                             label: (
-//                                 <NavLink to={`/${role}/${child.path}`}>
-//                                     {child.name}
-//                                 </NavLink>
-//                             ),
-//                         };
-//                     }
-//                 }),
-//             });
-//         }
-
-//         return acc;
-//     }, []);
-
-//     return sidebarItems;
-// };
 
 export const sideBarItemsGenerator = (items: TUserPath[], role: string) => {
     const sidebarItems = items.reduce((acc: TMenuItem[], item) => {
